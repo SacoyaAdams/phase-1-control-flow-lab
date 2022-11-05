2d32e7fe741cc1b9232b5c729d93fb91046ea1c5
Sacoya Adams <Sacoyaengineer@gmail.com>
2022-11-05 15:27:27 -0400

    commit tree 6622f9ccf7ec79264851274fa48fbf57f3da9c16

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if(someNumber <= 400)
+    return "This one is on me!";
+ else
+  return "That will be twenty bucks.";
 }
+scuberGreetingForFeet(199);
 
 function ternaryCheckCity(){
   // Write your code here!
